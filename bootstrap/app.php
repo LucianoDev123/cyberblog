e070d7ca3e3cc1a993386e2c76195a6635869e1e
LucianoDev123 <lucianolobos97@hotmail.com>
2026-07-22 22:28:36 -0300

    Conf inicial de composer y autoload mvc

---
+++ bootstrap/app.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use App\Core\App;
+
+$app = new App();
+
+return $app;
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'luciano/cyberblog',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0dfb7aee78508cd4df8fdc4d8ae10a35149edc97',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'luciano/cyberblog' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0dfb7aee78508cd4df8fdc4d8ae10a35149edc97',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
